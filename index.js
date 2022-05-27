252770a4e8c552068bc352ded208f84d8d3e71d9
Sean Booker Jr <seanbookerjunior@gmail.com>
2022-05-27 10:47:45 -0400

    commit tree f052f0224cfcb436131eb1867adb801c29c8e189

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solutions in this file
+//for ([initialization]; [condition]; [iteration]) {
+ //   [loop body]
+//}
+
+//for (let age = 30 ; age < 40; age++) {
+    //console.log(`I'm ${age} years old. Happy birthday to me!`);
+ //   debugger;
+//}
+
+//const gifts = ["teddy bear", "drone", "doll"];
+
+/*function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`)
+
+        debugger
+    }
+    return gifts
+    }
+wrapGifts(gifts)
+
+*/
+
+function writeCards(names , event) {
+    const newArray = []
+    for (let i = 0; i < names.length; i++) {
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+
+    }
+
+return newArray
+}
+
+
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--)
+    }
+    
+}
+
